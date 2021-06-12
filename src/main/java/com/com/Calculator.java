0e85a4f737699d015a18d667bfc6638c41568b7b
Alexey <reni.gate@gmail.com>
2021-06-12 17:25:46 +0500

    delete sout

--- src/main/java/com/com/Calculator.java
+++ src/main/java/com/com/Calculator.java
@@ -5,10 +5,7 @@ public class Calculator {
     public void start() {
         System.out.println("calc started");
         System.out.println("2 line");
-        System.out.println("calc stopped");
-        System.out.println("for 1 commit");
-        System.out.println("for 2 commit");
-        System.out.println("for 3 commit");
+
 
     }
 }
