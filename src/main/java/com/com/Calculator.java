e6824cf905f8aaffc089f1e4de7d31964c138fdd
Alexey <reni.gate@gmail.com>
2021-06-12 17:26:25 +0500

    added 1

--- src/main/java/com/com/Calculator.java
+++ src/main/java/com/com/Calculator.java
@@ -5,7 +5,9 @@ public class Calculator {
     public void start() {
         System.out.println("calc started");
         System.out.println("2 line");
-
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
 
     }
 }
