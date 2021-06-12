c9c429921ceedf5fda7672e19744dc6da314cfee
Alexey <reni.gate@gmail.com>
2021-06-12 15:08:32 +0500

    added 3 lines

--- src/main/java/com/com/Calculator.java
+++ src/main/java/com/com/Calculator.java
@@ -3,6 +3,8 @@ package com.com;
 public class Calculator {
 
     public void start() {
-
+        System.out.println("calc started");
+        System.out.println("2 line");
+        System.out.println("calc stopped");
     }
 }
